5022a0fc6e7a0d99ef69cae88c6673fb0d210fa3
Jonatan Silva <elundevtaker@gmail.com>
2023-10-09 10:31:05 -0300

    Trabajando con tipos de strings

---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,12 @@
+const producto = 'Monitor 20';
+
+console.log(producto);
+
+//conocer la cantidad de letras de la cadena de texto
+console.log(producto.length);
+
+console.log(producto.indexOf('tablet'))
+
+console.log(producto.includes('Monitor'))
+console.log(producto.includes('Tablet'))
+console.log(producto.includes('monitor'))
---
+++ 03-Strings/js/03-app.js
@@ -0,0 +1,9 @@
+const producto = 'Monitor 20';
+const precio = '30 USD'
+
+console.log(producto.concat(precio));
+//console.log(producto.concat('en descuento'));
+
+//console.log(producto + "concatenado con texto");
+
+console.log(`el producto ${producto} tiene un precio de${precio}`)
